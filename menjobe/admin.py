9a592325c8d25953016832ccb0683827a9bf2685
David García Garzón <voki@canvoki.net>
2014-08-29 23:21:10 +0200

    Exposing menjobe tables to admin interface

--- menjobe/admin.py
+++ menjobe/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
+from .models import RetailPoint
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(RetailPoint)
+
+
