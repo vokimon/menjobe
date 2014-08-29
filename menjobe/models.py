6c34f62116c2062ec2a6f37dc09fdf10dff52f91
David García Garzón <voki@canvoki.net>
2014-08-29 02:04:49 +0200

    Added Product table

--- menjobe/models.py
+++ menjobe/models.py
@@ -4,3 +4,8 @@ class DistributionPoint(models.Model) :
 	"""Groceries Distribution Point"""
 
 
+class Product(models.Model) :
+	name = models.CharField(max_length=200)
+
+
+
--- menjobe/tests.py
+++ menjobe/tests.py
@@ -1,4 +1,16 @@
 from django.test import TestCase
 
+from .models import Product
+
+class Product_Test(TestCase) :
+	def test_name(self) :
+		p = Product(name="Tomato")
+		self.assertEqual(p.name, "Tomato")
+		p.save()
+		self.assertEqual(p.id, 1)
+
+
+
+
 
 
