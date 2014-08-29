51636d9a7f83127838e23cdd6178ad5557a4c19d
David García Garzón <voki@canvoki.net>
2014-08-29 23:20:53 +0200

    urls for json services

--- devsite/urls.py
+++ devsite/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'devsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.devsite.urls)),
+    url(r'^json/allproducts$', 'menjobe.views.allProducts'),
+    url(r'^json/productretailers/([0-9]+)$', 'menjobe.views.retailersForProduct'),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
