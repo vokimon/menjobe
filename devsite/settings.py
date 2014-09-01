03b6e57d81a54c8688fadd007625cec4870f4277
David García Garzón <voki@canvoki.net>
2014-09-01 21:38:06 +0200

    Bootstrap based admin

--- devsite/settings.py
+++ devsite/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped.bootstrap3',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -37,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'menjobe',
+#    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
